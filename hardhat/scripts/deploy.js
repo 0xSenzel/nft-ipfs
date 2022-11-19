b2e4232f8d3c82b0896c02d5068abb6660b423c9
0xsenzel <0xsenzel@gmail.com>
2022-11-19 10:11:18 +0800

    initial commit

---
+++ hardhat/scripts/deploy.js
@@ -0,0 +1,27 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  // URL from where we can extract the metadata for a LW3Punks
+  const metadataURL = "ipfs://Qmbygo38DWF1V8GttM1zy89KzyZTPU2FLUzQtiDvB7q6i5/";
+  /*
+  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+  so lw3PunksContract here is a factory for instances of our LW3Punks contract.
+  */
+  const lw3PunksContract = await ethers.getContractFactory("LW3Punks");
+
+  // deploy the contract
+  const deployedLW3PunksContract = await lw3PunksContract.deploy(metadataURL);
+
+  await deployedLW3PunksContract.deployed();
+
+  // print the address of the deployed contract
+  console.log("LW3Punks Contract Address:", deployedLW3PunksContract.address); //0x17251A7647F50A316e393bFE12723d7854fFCBab
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
